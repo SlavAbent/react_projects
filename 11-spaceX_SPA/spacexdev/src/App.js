e7d4e66bd37ea2998a4e19e6e3720d97ba03cfa3
SlavAbent <slavabent2015@gmail.com>
2021-04-06 15:57:52 +0500

    addedhtml spacex

---
+++ 11-spaceX_SPA/spacexdev/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './style.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Features from './components/Features'
+import Footer from './components/Footer'
+import Calendar from './components/Calendar'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Features />
+      <Footer />
+      {/* <Calendar /> */}
+    </>
+  );
+}
+
+export default App;
