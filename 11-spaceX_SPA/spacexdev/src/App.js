9b74c68eaf6487108a0572c429267c17f29aef04
SlavAbent <slavabent2015@gmail.com>
2021-04-07 16:59:06 +0500

    create fetchdata

--- 11-spaceX_SPA/spacexdev/src/App.js
+++ 11-spaceX_SPA/spacexdev/src/App.js
@@ -5,17 +5,51 @@ import Main from './components/Main'
 import Features from './components/Features'
 import Footer from './components/Footer'
 import Calendar from './components/Calendar'
+import Details from './components/Details'
+import FetchData from './service/FetchData'
 
-function App() {
-  return (
-    <>
-      <Header />
-      <Main />
-      <Features />
-      <Footer />
-      {/* <Calendar /> */}
-    </>
-  );
+class App extends React.Component {
+
+  fetchData = new FetchData()
+  state = {
+    rocket: 'Falcon 1',
+    rocketFeatures: null,
+    rockets: []
+  }
+
+  componentDidMount(){
+    this.updateRockets()
+  }
+
+  updateRockets(){
+    this.fetchData.getRocket()
+      .then(data => {
+        this.setState({ rockets: data.map(item => item.name) })
+        return data
+      })
+      .then(data => data.find(item => item.name === this.state.rocket))
+      .then(rocketFeatures => this.setState({rocketFeatures}))
+  }
+  
+  changeRocket = rocket => {
+    this.setState({
+      rocket
+    }, this.updateRockets)
+  }
+
+  render() {
+
+    return (
+      <>
+        <Header rockets={this.state.rockets} changeRocket={this.changeRocket}/>
+        <Main rocket={this.state.rocket}/>
+        <Features />
+        <Footer />
+        <Calendar />
+        <Details />
+      </>
+    );
+  }
 }
 
 export default App;
